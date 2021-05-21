adfd64e1c74732af36fff1f4661abe1534ef7a5b
Sliphy <52437520+jimy189@users.noreply.github.com>
2021-05-21 07:36:43 -0300

    melhora

--- jogoHtml/js/main.js
+++ jogoHtml/js/main.js
@@ -1,11 +1,78 @@
 
+
 function start() {
     $("#inicio").hide();
 
-    $("#fundoGame").append("<div id='jogador'></div>");
-    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
-    $("#fundoGame").append("<div id='amigo'></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     
+
+    var jogo = {}
+    var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68  
+    }
+
+    jogo.pressionou = {};
+
+    $ (document).keydown(function(e){
+       jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e){
+      jogo.pressionou[e.which] = false;
+   });
+
+     jogo.timer =setInterval(loop,30);
+
+     function loop() {
+
+        movefundo();
+        movejogador();
+     }
+
+     function movefundo() {
+
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda-1);
+     }
+
+     function movejogador() {
+      // move para cima
+      if (jogo.pressionou[TECLA.W]) {
+         var topo = parseInt($("#jogador").css("top"));
+          $("#jogador").css("top", topo-10);
+         
+            if (topo<=0){
+               $("#jogador").css("top",topo+10);
+            }
+            
+         }
+
+      // move para baixo
+      if (jogo.pressionou[TECLA.S]) {
+          var topo = parseInt($("#jogador").css("top"));
+          $("#jogador").css("top", topo+10);
+
+          if (topo>=434){
+            $("#jogador").css("top",topo-10);
+         }
+         
+          
+      }
+
+
+      // move para direita
+      if (jogo.pressionou[TECLA.D]) {
+        
+      }
+
+     
+  } 
+
+     
 
 }
